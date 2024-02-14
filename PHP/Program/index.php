3e078ecae40bf8db2a8a9ff0705451c818fa99b8
sghda <salmaghaida@upi.edu>
2024-02-14 23:46:41 +0700

    php

--- PHP/Program/index.php
+++ PHP/Program/index.php
@@ -5,12 +5,14 @@ class DPR {
     public $nama;
     public $bidang;
     public $partai;
+    public $foto;
 
-    function __construct($id=0, $nama="", $bidang="", $partai="") {
+    function __construct($id=0, $nama="", $bidang="", $partai="", $foto="") {
         $this->id = $id;
         $this->nama = $nama;
         $this->bidang = $bidang;
         $this->partai = $partai;
+        $this->foto = $foto;
     }
 }
 
@@ -21,68 +23,69 @@ class CRUD {
         $this->anggota_dpr = [];
     }
 
-    function tambah_anggota_dpr($nama, $bidang, $partai) {
+    function tambah_anggota_dpr($nama, $bidang, $partai, $foto) {
         $id = count($this->anggota_dpr) + 1;
-        array_push($this->anggota_dpr, new DPR($id, $nama, $bidang, $partai));
+        array_push($this->anggota_dpr, new DPR($id, $nama, $bidang, $partai, $foto));
     }
 
     function hapus_anggota_dpr($id) {
         foreach ($this->anggota_dpr as $key => $anggota) {
             if ($anggota->id == $id) {
                 unset($this->anggota_dpr[$key]);
-                echo "\n <!> Data anggota DPR dengan ID $id berhasil dihapus <!>\n";
+                echo "<br> <!> Data anggota DPR dengan ID $id berhasil dihapus <!><br>";
                 return;
             }
         }
-        echo "\n <!> Data anggota DPR dengan ID $id tidak ditemukan <!>\n";
+        echo "<br> <!> Data anggota DPR dengan ID $id tidak ditemukan <!><br>";
     }
 
-    function edit_anggota_dpr($id, $nama, $bidang, $partai) {
+    function edit_anggota_dpr($id, $nama, $bidang, $partai, $foto) {
         foreach ($this->anggota_dpr as $anggota) {
             if ($anggota->id == $id) {
                 $anggota->nama = $nama;
                 $anggota->bidang = $bidang;
                 $anggota->partai = $partai;
-                echo "\n <!> Data anggota DPR dengan ID $id berhasil diedit <!>\n";
+                $anggota->foto = $foto;
+                echo "<br> <!> Data anggota DPR dengan ID $id berhasil diedit <!><br>";
                 return;
             }
         }
-        echo "\n <!> Data anggota DPR dengan ID $id tidak ditemukan <!>\n";
+        echo "<br> <!> Data anggota DPR dengan ID $id tidak ditemukan <!><br>";
     }
 
     function tampilkan_anggota_dpr() {
-        echo"\n >>> Data Anggota DPR <<<\n";
+        echo"<br> >>> Data Anggota DPR <<< <br>";
         // Menampilkan header tabel
-        echo " +-----+----------------------+------------+------------+\n";
-        printf(" | %-3s | %-20s | %-10s | %-10s |\n", "ID", "Nama", "Bidang", "Partai");
+        echo " +-----+----------------------+------------+------------+---------------------+<br>";
+        printf(" | %-3s | %-20s | %-10s | %-10s | %-20s |<br>", "ID", "Nama", "Bidang", "Partai", "Foto");
 
         // Menampilkan baris pemisah
-        echo " +-----+----------------------+------------+------------+\n";
+        echo " +-----+----------------------+------------+------------+---------------------+<br>";
         
         // Menampilkan data anggota DPR
         foreach ($this->anggota_dpr as $anggota) {
-            printf(" | %-3d | %-20s | %-10s | %-10s |\n", $anggota->id, $anggota->nama, $anggota->bidang, $anggota->partai);
+            printf(" | %-3d | %-20s | %-10s | %-10s | <img src='%s' alt='%s' width='100' height='100'> <br> |<br>", $anggota->id, $anggota->nama, $anggota->bidang, $anggota->partai, $anggota->foto, $anggota->nama);
         }
 
         // Menampilkan baris pemisah
-        echo " +-----+----------------------+------------+------------+\n";
+        echo " +-----+----------------------+------------+------------+---------------------+<br>";
     }
 }
 
 // Membuat objek CRUD
 $crud = new CRUD();
 
-echo "Menambahkan data anggota DPR\n";
+echo "<br>Menambahkan data anggota DPR<br>";
 // Menambahkan data anggota DPR secara hardcode
-$crud->tambah_anggota_dpr("Anggota 1", "Bidang 1", "Partai 1");
-$crud->tambah_anggota_dpr("Anggota 2", "Bidang 2", "Partai 2");
-$crud->tambah_anggota_dpr("Anggota 3", "Bidang 3", "Partai 3");
+$crud->tambah_anggota_dpr("Anggota 1", "Bidang 1", "Partai 1", "foto1.png");
+$crud->tambah_anggota_dpr("Anggota 2", "Bidang 2", "Partai 2", "foto2.png");
+$crud->tambah_anggota_dpr("Anggota 3", "Bidang 3", "Partai 3", "foto3.png");
 
 // Menampilkan data anggota DPR
 $crud->tampilkan_anggota_dpr();
 
 //edit
-$crud->edit_anggota_dpr(2, "Anggota B", "Bidang B", "Partai B");
+$crud->edit_anggota_dpr(2, "Anggota B", "Bidang B", "Partai B", "foto_b.png");
 
 // Menampilkan data anggota DPR
 $crud->tampilkan_anggota_dpr();
@@ -92,6 +95,5 @@ $crud->hapus_anggota_dpr(2);
 
 // Menampilkan data anggota DPR
 $crud->tampilkan_anggota_dpr();
-
 
 ?>
